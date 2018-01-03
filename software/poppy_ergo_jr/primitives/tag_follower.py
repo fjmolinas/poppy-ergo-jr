75254d500ef77af0e32cd3b8aa373357a6c9c733
rdelgadov <rdelgadovega@gmail.com>
2018-01-03 17:11:26 -0300

    Solve problems, code working in jupyter notebook

--- software/poppy_ergo_jr/primitives/tag_follower.py
+++ software/poppy_ergo_jr/primitives/tag_follower.py
@@ -1,4 +1,4 @@
-from cv2 import aruco
+import cv2
 import numpy as np
 import json
 import os
@@ -14,8 +14,7 @@ class TagFollower(LoopPrimitive):
             base_path = os.path.abspath(os.path.join(os.path.dirname(__file__),os.pardir))
             data_path = os.path.join(os.path.join(base_path, 'configuration'), 'camera_calibration.json')
             data = json.load(open(data_path))
-            matrix = np.array([np.array(xi) for xi in data['mtx']])
-            return matrix, np.array(data['dist'])
+            return np.float64(data['mtx']), np.float64(data['dist'])
 
         self.camera_parameters = camera_parameters
     def setup(self):
@@ -33,9 +32,8 @@ class TagFollower(LoopPrimitive):
             m.led = 'green'
 
         """Camera & aruco setup"""
-
-        self.dictionary = aruco.getPredefinedDictionary(aruco.DICT_5X5_250)
-        self.matrix,self.distribution = self.camera_parameters()
+        self.dictionary = cv2.aruco.getPredefinedDictionary(aruco.DICT_5X5_250)
+        self.camera_matrix,self.distrib = self.camera_parameters()
         self.M = np.eye(4)
         self.kwargs = {}
         self.angle = np.pi/3
@@ -43,12 +41,12 @@ class TagFollower(LoopPrimitive):
 
     def update(self):
         frame = self.robot.camera.frame
-        marker = aruco.detectMarkers(frame,self.dictionary)
+        marker = cv2.aruco.detectMarkers(frame,self.dictionary)
         if marker[0]:
-            estimated_pose = aruco.estimatePoseSingleMarkers(marker[0],0.027,self.matrix,self.distribution)
-            position = estimated_pose[1][0][0]
-            self.M[:3,3] = (position[0], -1.*position[3]*np.cos(self.angle), position[3]*np.sin(self.angle)-position[2]*np.sin(np.pi/6))
-            inverse = np.round(self.robot.chain.inverse_kinematics(self.M, initial_position=self.robot.chain.convert_to_ik_angles(self.robot.chain.joints_position),**self.kwargs),3)
+            rvecs,tvecs,objects = cv2.aruco.estimatePoseSingleMarkers(marker[0],0.027,self.camera_matrix,self.distrib)
+            position = tvecs[0][0]
+            self.M[:3,3] = (position[0],-1.*position[2]*np.sin(self.angle)-position[1]*np.sin(np.pi/2 - self.angle),position[2]*np.cos(self.angle))#(position[0], position[2]*np.cos(np.pi/3), position[2]*np.sin(np.pi/3)-position[1]*np.sin(np.pi/6))
+            inverse = np.round(self.robot.chain.inverse_kinematics(self.M, initial_position=self.robot.chain.convert_to_ik_angles(self.robot.chain.joints_position), **kwargs),3)
             inverse_ik = self.robot.chain.convert_from_ik_angles(inverse)
             for i in range(len(self.robot.motors)):
                 self.robot.motors[i].goto_position(inverse_ik[i],1)
@@ -58,4 +56,4 @@ class TagFollower(LoopPrimitive):
 
         for m in self.robot.motors:
             m.led = 'off'
-	    m.compliant = True
+	        m.compliant = True
