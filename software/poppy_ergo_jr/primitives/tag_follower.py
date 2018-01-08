e80f76f34982c019b8eaf875b4724658b2ce4fb3
rdelgadov <rdelgadovega@gmail.com>
2018-01-08 13:15:16 +0000

    added some logs to get time

--- software/poppy_ergo_jr/primitives/tag_follower.py
+++ software/poppy_ergo_jr/primitives/tag_follower.py
@@ -2,10 +2,15 @@ import cv2
 import json
 import numpy as np
 import os
+import logger
+import time
 
 
 from pypot.primitive import LoopPrimitive
 from .postures import IdlePosture
+
+logger = logging.getLogger(__name__)
+
 
 class TagFollower(LoopPrimitive):
     def __init__(self, robot):
@@ -51,16 +56,23 @@ class TagFollower(LoopPrimitive):
         """
             Search in robot camera all the aruco markers and take the first to follow.
         """
+        init_time = time.clock()
         frame = self.robot.camera.frame
+        logger.info("time spent in get camera frame: {}".format((time.clock()-init_time)/1000.))
         marker = cv2.aruco.detectMarkers(frame,self.dictionary)
+        logger.info("time spent in detect marker: {}".format((time.clock()-init_time)/1000.))
         if marker[0]:
             rvecs,tvecs,objects = cv2.aruco.estimatePoseSingleMarkers(marker[0],self.marker_lenght,self.camera_matrix,self.distrib)
+            logger.info("time spent in estimate pose: {}".format((time.clock()-init_time)/1000.))
             position = tvecs[0][0]
             self.M[:3,3] = (position[0],-1.*position[2]*np.sin(self.angle)-position[1]*np.sin(np.pi/2 - self.angle),position[2]*np.cos(self.angle))
             inverse = np.round(self.robot.chain.inverse_kinematics(self.M, initial_position=self.robot.chain.convert_to_ik_angles(self.robot.chain.joints_position), **self.kwargs),3)
+            logger.info("time spent in get ik: {}".format((time.clock()-init_time)/1000.))
             inverse_ik = self.robot.chain.convert_from_ik_angles(inverse)
             for i in range(len(self.robot.motors)):
                 self.robot.motors[i].goto_position(inverse_ik[i],1)
+            logger.info("time spent in move to position: {}".format((time.clock()-init_time)/1000.))
+
     def teardown(self):
         idle_prim = IdlePosture(self.robot, 3.0)
         idle_prim.start()
